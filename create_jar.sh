e6e1218ba7b1e056fd963baddd005177aee209e6
Denis Rodikov <Denis.Rodikov@transas.com>
2018-05-07 20:47:50 +0300

    Open first file pr

--- create_jar.sh
+++ create_jar.sh
@@ -3,9 +3,11 @@ rm -rf swingnavigator
 mkdir swingnavigator
 cp -r out/production/ui/ui swingnavigator
 cp -r out/production/core/folder swingnavigator
+cp -r out/production/core/file_preview swingnavigator
+cp -r out/production/core/folder_management swingnavigator
 cp -r out/production/thirdparty/thirdparty swingnavigator
 cd swingnavigator
-jar cvfe swingnavigator.jar ui.FolderNavigatorUI ui/*.class ui/folder_button/*.class ui/file_preview/*.class folder/*.class folder/file_preview/*.class folder/ftp_folder/*.class folder/factory/*.class folder/zip_folder/*.class thirdparty/*.class folder/file_preview/images/*.png
+jar cvfe swingnavigator.jar ui.FolderNavigatorUI ui/*.class ui/folder_button/*.class ui/file_preview/*.class folder/*.class folder_management/*.class file_preview/*.class folder/ftp_folder/*.class folder/factory/*.class folder/zip_folder/*.class thirdparty/*.class file_preview/images/*.png
 mv swingnavigator.jar ..
 cd ..
 rm -rf swingnnavigator
