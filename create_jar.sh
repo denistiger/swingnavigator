0b6d9a5d8cf92ae69f79d61149bb066e62fc663f
Denis Rodikov <Denis.Rodikov@transas.com>
2018-05-07 22:35:43 +0300

    Adding apache ftp server to own jar file

--- create_jar.sh
+++ create_jar.sh
@@ -6,8 +6,11 @@ cp -r out/production/core/folder swingnavigator
 cp -r out/production/core/file_preview swingnavigator
 cp -r out/production/core/folder_management swingnavigator
 cp -r out/production/thirdparty/thirdparty swingnavigator
+cp third-party/commons-net-3.6/commons-net-3.6.jar swingnavigator
 cd swingnavigator
-jar cvfe swingnavigator.jar ui.FolderNavigatorUI ui/*.class ui/folder_button/*.class ui/file_preview/*.class folder/*.class folder_management/*.class file_preview/*.class folder/ftp_folder/*.class folder/factory/*.class folder/zip_folder/*.class thirdparty/*.class file_preview/images/*.png
+unzip commons-net-3.6.jar 
+rm commons-net-3.6.jar
+jar cvfe swingnavigator.jar ui.FolderNavigatorUI ui/*.class ui/folder_button/*.class ui/file_preview/*.class folder/*.class folder_management/*.class file_preview/*.class folder/ftp_folder/*.class folder/factory/*.class folder/zip_folder/*.class thirdparty/*.class file_preview/images/*.png $(find org -name '*.class')
 mv swingnavigator.jar ..
 cd ..
 rm -rf swingnnavigator
