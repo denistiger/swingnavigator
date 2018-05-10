0724c466ee51e303800ce032e0f8699b799c6166
denis <denisrodikov@gmail.com>
2018-05-10 13:23:26 +0300

    Correted jar generation for windows. ftp client is not added.

--- create_jar.bat
+++ create_jar.bat
@@ -1,10 +1,14 @@
 rd /s /q swingnavigator
 mkdir swingnavigator
-xcopy out\production\ui\ui swingnavigator\ui /O /X /E /H /K /I
-xcopy out\production\core\folder swingnavigator\folder /O /X /E /H /K /I
-xcopy out\production\thirdparty\thirdparty swingnavigator\thirdparty /O /X /E /H /K /I
+xcopy out\production\ui\ui swingnavigator\ui /E /H /K /I
+xcopy out\production\core\folder swingnavigator\folder /E /H /K /I
+xcopy out\production\core\folder_management swingnavigator\folder_management /E /H /K /I
+xcopy out\production\core\file_preview swingnavigator\file_preview /E /H /K /I
+xcopy out\production\thirdparty\thirdparty swingnavigator\thirdparty /E /H /K /I
+xcopy third-party\commons-net-3.6\commons-net-3.6.jar swingnavigator
 cd swingnavigator
-"C:\Program Files\Java\jdk1.8.0_151\bin\jar.exe" cvfe swingnavigator.jar ui.FolderNavigatorUI ui\*.class ui\folder_button\*.class ui\file_preview\*.class folder\*.class folder\file_preview\*.class folder\ftp_folder\*.class folder\factory\*.class folder\zip_folder\*.class thirdparty\*.class folder\file_preview\images\*.png
+"%JAVA_HOME%\bin\jar.exe" xf commons-net-3.6.jar
+"%JAVA_HOME%\bin\jar.exe" cvfe swingnavigator.jar ui.FolderNavigatorUI ui\*.class ui\folder_button\*.class ui\file_preview\*.class folder\*.class folder_management/*.class file_preview\*.class folder\ftp_folder\*.class folder\factory\*.class folder\zip_folder\*.class thirdparty\*.class file_preview\images\*.png
 move swingnavigator.jar ..
 cd ..
-rd /s /q swingnavigator
+rem rd /s /q swingnavigator
