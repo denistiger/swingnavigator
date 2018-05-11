6bdb7c12e9c939738e4d064503ff2fe185fc7e7b
denis <denisrodikov@gmail.com>
2018-05-11 13:54:52 +0300

    Mind map for swing navigator

---
+++ docs/Swing Navigator.mm
@@ -0,0 +1,243 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1526029370523" ID="ID_406156507" MODIFIED="1526033632617" TEXT="Swing Navigator">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1526030036647" ID="ID_1340491878" MODIFIED="1526033705968" POSITION="right" TEXT="Supported Formats">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1526030052952" ID="ID_923700736" MODIFIED="1526033616560" TEXT="Standard folder">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526030078529" ID="ID_981675749" MODIFIED="1526033616560" TEXT="All supported formats except ftp inside">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526030062272" ID="ID_531773962" MODIFIED="1526033616560" TEXT="Zip folder">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526030166258" ID="ID_102607436" MODIFIED="1526033616560" TEXT="All supported formats except ftp inside">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526030174432" ID="ID_73752163" MODIFIED="1526033616560" TEXT="For example Zip in Zip in Zip...">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526030190169" ID="ID_709395586" MODIFIED="1526033616560" TEXT="For example Image in Zip in Zip">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526030250819" ID="ID_430046152" MODIFIED="1526033616560" TEXT="Extensions: zip, jar">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526030067503" ID="ID_880219898" MODIFIED="1526033616576" TEXT="FTP address">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526030213713" ID="ID_821844981" MODIFIED="1526033616576" TEXT="All supported formats except ftp inside">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526033232549" ID="ID_739511906" MODIFIED="1526033616576" TEXT="Login window is displayed if credentials are needed.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526033274630" ID="ID_535612753" MODIFIED="1526033616576" TEXT="Path examples">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1526033280622" ID="ID_771826713" MODIFIED="1526033616576" TEXT="ftp://address/"/>
+<node COLOR="#111111" CREATED="1526033322710" ID="ID_1873834594" MODIFIED="1526033616576" TEXT="ftp:/address:port/"/>
+<node COLOR="#111111" CREATED="1526033287614" ID="ID_508714987" MODIFIED="1526033616576" TEXT="ftp://login@address/"/>
+<node COLOR="#111111" CREATED="1526033305117" ID="ID_1276480562" MODIFIED="1526033616576" TEXT="ftp://login:password@address/"/>
+<node COLOR="#111111" CREATED="1526033316909" ID="ID_941967734" MODIFIED="1526033616576" TEXT="ftp://address/folder/folder/"/>
+<node COLOR="#111111" CREATED="1526033355086" ID="ID_1177486064" MODIFIED="1526033616576" TEXT="ftp://address/folder/file"/>
+<node COLOR="#111111" CREATED="1526033365294" ID="ID_1728724252" MODIFIED="1526033616576" TEXT="ftp://admin:admin@127.0.0.1:2121/folder/folder.zip/1.jpg"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526030089872" ID="ID_1258897921" MODIFIED="1526033616576" TEXT="Text file by extension">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526030262329" ID="ID_1285568547" MODIFIED="1526033616576" TEXT="css, qsi, py, qss, java, bat, sh, xml, thrift, json, cpp, c, mtl, ini, cfg, in, profile, h, pro, txt, obj, cmake, cmd">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526030099824" ID="ID_618140004" MODIFIED="1526033616576" TEXT="Image file by extension">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526030487395" ID="ID_1034803710" MODIFIED="1526033616576" TEXT="jpeg, jpg, png, tiff, bmp, gif">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526032245093" ID="ID_780605166" MODIFIED="1526033616576" STYLE="fork" TEXT="Fictive top level folder with the list of file systems roots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526032307326" ID="ID_1871372664" MODIFIED="1526033616576" TEXT="For Windows its a list of drives: C:\, D:\ etc">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1526029499878" ID="ID_897275133" MODIFIED="1526033701778" POSITION="left" TEXT="Navigation capabilities">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1526029449835" ID="ID_1468999396" MODIFIED="1526033616576" TEXT="By address">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526029655416" ID="ID_32655246" MODIFIED="1526033616576" TEXT="Standard mode">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1526029685944" ID="ID_1713722293" MODIFIED="1526033616576" TEXT="Press backspace to clear address"/>
+<node COLOR="#111111" CREATED="1526029713438" ID="ID_1479117148" MODIFIED="1526033616576" TEXT="Press Ctrl + A to clear address"/>
+<node COLOR="#111111" CREATED="1526029756975" ID="ID_721060927" MODIFIED="1526033616576" TEXT="Enter address by appending it to the current text field"/>
+<node COLOR="#111111" CREATED="1526029803607" ID="ID_1772036984" MODIFIED="1526033616576" TEXT="Cursor is always at the end of the line"/>
+</node>
+<node COLOR="#990000" CREATED="1526029681391" ID="ID_896440435" MODIFIED="1526033616576" TEXT="Edit Mode">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1526029833344" ID="ID_1689811367" MODIFIED="1526033616576" TEXT="Press the Edit Address button."/>
+<node COLOR="#111111" CREATED="1526029863888" ID="ID_1362822883" MODIFIED="1526033616576" TEXT="Edit address field as normal"/>
+<node COLOR="#111111" CREATED="1526029916399" ID="ID_263762705" MODIFIED="1526033616576" TEXT="Press the Go Address button to go address"/>
+<node COLOR="#111111" CREATED="1526029956416" ID="ID_1156365731" MODIFIED="1526033616576" TEXT="Press the Reset Button to return to current address"/>
+</node>
+<node COLOR="#990000" CREATED="1526033409294" ID="ID_1033395077" MODIFIED="1526033616576" TEXT="Delimiter">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1526033463775" ID="ID_1574773120" MODIFIED="1526033616576" TEXT="Platform specific for local paths">
+<node COLOR="#111111" CREATED="1526033471007" ID="ID_400829560" MODIFIED="1526033616576" TEXT="D:\Photo\2018"/>
+<node COLOR="#111111" CREATED="1526033480417" ID="ID_1500375391" MODIFIED="1526033616576" TEXT="/home/user/Desktop"/>
+</node>
+<node COLOR="#111111" CREATED="1526033493648" ID="ID_279226508" MODIFIED="1526033616576" TEXT="For ftp is /">
+<node COLOR="#111111" CREATED="1526033504151" ID="ID_1258795845" MODIFIED="1526033616576" TEXT="ftp://address/folder/folder/image.jpg"/>
+</node>
+<node COLOR="#111111" CREATED="1526033526832" ID="ID_711508822" MODIFIED="1526033616576" TEXT="Inside zip is /">
+<node COLOR="#111111" CREATED="1526033532657" ID="ID_1886841616" MODIFIED="1526033616576" TEXT="D:\Photo\2018\Photo.zip/testSet/image.jpg"/>
+<node COLOR="#111111" CREATED="1526033563055" ID="ID_658457348" MODIFIED="1526033616576" TEXT="/home/user/Desktop/testSet/image.jpg"/>
+<node COLOR="#111111" CREATED="1526033585623" ID="ID_467487456" MODIFIED="1526033616576" TEXT="ftp://address/folder/folder.zip/testSet/image.jpg"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526029458517" ID="ID_1519159869" MODIFIED="1526033616591" TEXT="Select folder by mouse">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526029639800" ID="ID_1220098029" MODIFIED="1526033616591" TEXT="Double click opens the folder">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526029484756" ID="ID_982959214" MODIFIED="1526033616591" TEXT="Go level up">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526029564087" ID="ID_1618689170" MODIFIED="1526033616591" TEXT="Press LevelUp button">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526029574109" ID="ID_196456615" MODIFIED="1526033616591" TEXT="Press Ctrl+BackSpace">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526029583990" ID="ID_1162385572" MODIFIED="1526033616591" TEXT="Press BackSpace if cursor is in the end of path text field">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526029610903" ID="ID_4748065" MODIFIED="1526033616591" TEXT="Press at the first folder &quot;..&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526029467725" ID="ID_263126695" MODIFIED="1526033616591" TEXT="Select folder by keyboard keys">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1526029412987" ID="ID_1545456088" MODIFIED="1526033616507" POSITION="left" TEXT="Files Viewing">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1526032416239" ID="ID_1318773439" MODIFIED="1526033616507" TEXT="When text or image file is opened viewing mode is enabled.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1526032446904" ID="ID_75007685" MODIFIED="1526033616522" TEXT="Text file view mode">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526032453391" ID="ID_1176387879" MODIFIED="1526033616522" TEXT="Simple text area">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032493785" ID="ID_1904966622" MODIFIED="1526033616522" TEXT="First megabyte of file is being read">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032541481" ID="ID_129021886" MODIFIED="1526033616522" TEXT="No notifications on large files">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526032555216" ID="ID_1938173578" MODIFIED="1526033616522" TEXT="Image file view mode">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526032874187" ID="ID_1849133958" MODIFIED="1526033616522" TEXT="Show image in the available space">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032894291" ID="ID_412006695" MODIFIED="1526033616539" TEXT="Full Screen">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1526032901106" ID="ID_54318937" MODIFIED="1526033616539" TEXT="Ctrl + F to enter"/>
+<node COLOR="#111111" CREATED="1526032935050" ID="ID_1684040377" MODIFIED="1526033616540" TEXT="Ctrl + F, Esc, Backspace to exit"/>
+<node COLOR="#111111" CREATED="1526033106460" ID="ID_651354850" MODIFIED="1526033616540" TEXT="Left and Right arrows for navigation"/>
+<node COLOR="#111111" CREATED="1526033120596" ID="ID_325295615" MODIFIED="1526033616540" TEXT="Text files or unknown files are shown in a non-full screen mode"/>
+<node COLOR="#111111" CREATED="1526033204420" ID="ID_1512735773" MODIFIED="1526033616540" TEXT="Auto restore when come to image"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526032596849" ID="ID_1666561703" MODIFIED="1526033616542" TEXT="Layout">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526032600673" ID="ID_1115529877" MODIFIED="1526033616542" TEXT="Current file large preview">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032648513" ID="ID_45822287" MODIFIED="1526033616543" TEXT="Current file name">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032611369" ID="ID_1767278570" MODIFIED="1526033616543" TEXT="Previous and next files icons at the top">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032633642" ID="ID_1450295753" MODIFIED="1526033616544" TEXT="Left and right buttons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032718587" ID="ID_814558621" MODIFIED="1526033616544" TEXT="Special indication for the start and the end of the folder">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526032955452" ID="ID_14926442" MODIFIED="1526033616544" TEXT="Navigation">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526032958614" ID="ID_282714731" MODIFIED="1526033616544" TEXT="Left and Right arrow">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032985899" ID="ID_755163686" MODIFIED="1526033616544" TEXT="Prev and Next buttons">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032969197" ID="ID_1252921486" MODIFIED="1526033616544" TEXT="Ctrl + F to images full screen">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526033006875" ID="ID_1839960897" MODIFIED="1526033616544" TEXT="Ctrl + Backspace, Backspace, LevelUp button to exit file viewing">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1526030576211" ID="ID_1098762298" MODIFIED="1526033616544" POSITION="right" TEXT="Folder Icons">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1526030612245" ID="ID_656615723" MODIFIED="1526033616544" TEXT="Text file icon is up to 6 lines 16 symbols each. Tabs, extra spaces are removed">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1526031787412" ID="ID_1018712092" MODIFIED="1526033616544" TEXT="Image file icon is a resized image which is put inside 96*96 pixels frame">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1526031843197" ID="ID_1002776110" MODIFIED="1526033616544" TEXT="Local folder or zip icon">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1526031873364" ID="ID_1408768262" MODIFIED="1526033616544" TEXT="Background with folder or zip folder icon">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526031891972" ID="ID_1969205392" MODIFIED="1526033616544" TEXT="Foreground with the icon of the first image inside the folder scaled to 60%">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1526032198821" ID="ID_1825797818" MODIFIED="1526033616544" TEXT="Foreground part is loading in background an appears after some time">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1526032083916" ID="ID_789421575" MODIFIED="1526033616560" TEXT="Unknown file format. Icon with a Question sign on it">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1526032121038" ID="ID_1065251747" MODIFIED="1526033616560" TEXT="Icons for files and images are loading in background">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
