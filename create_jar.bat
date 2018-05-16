bcc18e673f779c13612538f1bd8d8ae5e5eb1cf4
denis <denisrodikov@gmail.com>
2018-05-16 19:52:48 +0300

    Script for creating .jar under Windows. Current directory path length is hardcoded.

--- create_jar.bat
+++ create_jar.bat
@@ -1,3 +1,4 @@
+rem @echo off
 rd /s /q swingnavigator
 mkdir swingnavigator
 xcopy out\production\ui\ui swingnavigator\ui /E /H /K /I
@@ -10,21 +11,21 @@ cd swingnavigator
 "%JAVA_HOME%\bin\jar.exe" xf commons-net-3.6.jar
 set files_list=
 setlocal enabledelayedexpansion
-set cut=%CD%
-echo cut
 for /D /r org %%x in ("*") do (
-	for /F %%i in ('dir /b %%x"\*.*"') do (
-	   echo "Local file: "%%i
-	   set tmp=%%x
-	   set files_list=!files_list! !tmp:~59!\*.*
-	   goto NextFolder
+    set f=0
+    for %%s in (%%x\*.class) do (
+	if !f! EQU 0 (
+        	echo %%s
+		set cutoff=%%x
+	        set files_list=!files_list! !cutoff:~59!\*.class
+		set f=1
 	)
-	:NextFolder	
-	echo "Folder done "%%x
+    )
 )
 echo !files_list!
 "%JAVA_HOME%\bin\jar.exe" cvfe swingnavigator.jar ui.FolderNavigatorUI ui\*.class ui\folder_button\*.class ui\file_preview\*.class folder\*.class folder_management/*.class file_preview\*.class folder\ftp_folder\*.class folder\factory\*.class folder\zip_folder\*.class thirdparty\*.class file_preview\images\*.png %files_list%
 endlocal
 move swingnavigator.jar ..
 cd ..
-rem rd /s /q swingnavigator
+rd /s /q swingnavigator
+
